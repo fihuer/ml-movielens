0b0d5231cfb789cf59c627a425a01c2622ca8c83
Fihue Romain <romain.fihue@student.ecp.fr>
2015-03-10 17:10:12 +0100

    Cleanly killing process on interrupt

--- pearson-sim.py
+++ pearson-sim.py
@@ -6,55 +6,62 @@ from multiprocessing import Process, Manager, Pool
 import time, math
 from Queue import Full, Empty
 from threading import Thread
+from clint.textui import progress
+
 
 def pearson(in_queue, out_queue):
-    while True:
-        try:
-            args = in_queue.get(block=True, timeout=1)
-        except Empty:
-            #print "In Queue empty"
-            return
-        x = args[0]
-        y = args[1]
-        xmean = args[2]
-        ymean = args[3]
-        data = args[4]
+    try:
+        while True:
+            try:
+                args = in_queue.get(block=True, timeout=1)
+            except Empty:
+                #print "In Queue empty"
+                return
+            x = args[0]
+            y = args[1]
+            xmean = args[2]
+            ymean = args[3]
+            data = args[4]
 
-        bottom_x_sq = 0
-        bottom_y_sq = 0
-        sum_top = 0
-        for movie, ratings in data.iteritems():
-            if ((x in ratings) and (y in ratings)):
-                sum_top += (ratings[x]-xmean)*(ratings[y]-ymean)
-                bottom_x_sq += pow(ratings[x]-xmean, 2)
-                bottom_y_sq += pow(ratings[y]-ymean, 2)
-        bottom_x = math.sqrt(bottom_x_sq)
-        bottom_y = math.sqrt(bottom_y_sq)
-        if((bottom_x*bottom_y) != 0):
-            result = sum_top / (bottom_x*bottom_y)
-        else:
-            result = 0
-        out_queue.put((x, y, result), block=True, timeout=1)
+            bottom_x_sq = 0
+            bottom_y_sq = 0
+            sum_top = 0
+            for movie, ratings in data.iteritems():
+                if ((x in ratings) and (y in ratings)):
+                    sum_top += (ratings[x]-xmean)*(ratings[y]-ymean)
+                    bottom_x_sq += pow(ratings[x]-xmean, 2)
+                    bottom_y_sq += pow(ratings[y]-ymean, 2)
+            bottom_x = math.sqrt(bottom_x_sq)
+            bottom_y = math.sqrt(bottom_y_sq)
+            if((bottom_x*bottom_y) != 0):
+                result = sum_top / (bottom_x*bottom_y)
+            else:
+                result = 0
+            out_queue.put((x, y, result), block=True, timeout=1)
+    except KeyboardInterrupt:
+        return
 
 def feed(args):
-    xRange = args[0]
-    users = args[1]
-    dataset = args[2]
-    in_queue=args[3]
-    mean = args[4]
+    try:
+        xRange = args[0]
+        users = args[1]
+        dataset = args[2]
+        in_queue=args[3]
+        mean = args[4]
 
-    for x in xRange:
-        for y in users[x:]:
-            xmean = mean[x]
-            ymean = mean[y]
-            filtereddata = {movie: {user:  dataset.data[movie][user] for user in dataset.data[movie] if (user == x or user == y)} for \
-movie in dataset.data}
-            filtereddata = {movie: filtereddata[movie] for movie in filtereddata if filtereddata[movie]}
-            try:
+        for x in xRange:
+            for y in users[x:]:
+                xmean = mean[x]
+                ymean = mean[y]
+                filtereddata = {movie: {user:  dataset.data[movie][user] for user in dataset.data[movie] if (user == x or user == y)} for movie in dataset.data}
+                filtereddata = {movie: filtereddata[movie] for movie in filtereddata if filtereddata[movie]}
                 in_queue.put((x,y, xmean, ymean, filtereddata), block=True, timeout=10)
-            except Full:
-                print ''
-                print "In Queue's full"
+    except Full:
+        print ''
+        print "In Queue's full"
+    except KeyboardInterrupt:
+        return
+
 
 class MlSimMatrix:
     def __init__(self):
@@ -69,21 +76,25 @@ class MlSimMatrix:
         matrix = self.__gatherResults__()
         self.__stopWorkers__()
         return matrix
-        
+
     def __chunks__(self, csize):
         for i in xrange(0, self.matrixSize, csize):
             yield [self.dataset.users(self.matrixSize)[i:i+csize], self.dataset.users(self.matrixSize), self.dataset ,self.in_queue, self.dataset.mean]
-    
+
 
     def __startFeeder__(self, nbfeeders=4):
         #thread = Thread(target=self.__feedWorkers__, args=())
         #thread.daemon = True
         #thread.start()
-        p = Pool(nbfeeders)
+        self.feederPool = Pool(nbfeeders)
         chunks = tuple(self.__chunks__(self.matrixSize/nbfeeders))
 #        print chunks
-        p.map_async(feed, chunks)
+        self.feederPool.map_async(feed, chunks)
         self.feederLaunched = True
+
+    def __stopFeeder__(self):
+        self.feederPool.terminate()
+        self.feederPool.join()
 
     def __gatherResults__(self):
         while not self.feederLaunched:
@@ -95,16 +106,23 @@ class MlSimMatrix:
         matrix = core.SymMatrix(self.matrixSize)
         to_do = pow(self.matrixSize,2)/float(2)
         done = 0
-        while True:
-            try:
-                result = self.out_queue.get(block=True, timeout=2)
-                done += 1
-            except Empty:
-                break
-            matrix[result[0], result[1]] = result[2]
-            if done%100==0:
-                print str(done/to_do)+'\r\r',
+        workersKilled = False
+        with progress.Bar(label="Distance Matrix", expected_size=to_do) as bar:
+            while True:
+                try:
+                    result = self.out_queue.get(block=True, timeout=2)
+                    done += 1
+                    matrix[result[0], result[1]] = result[2]
+                    #print 'Distance matrix compuation {:10.4}% done\r\r'.format((done/to_do)*100),
+                    if done <= to_do:
+                        bar.show(done)
+                except Empty:
+                    break
         return matrix
+
+    def kill(self):
+        self.__stopFeeder__()
+        self.__stopWorkers__()
 
     def __stopWorkers__(self):
         for p in self.__threads:
@@ -120,8 +138,8 @@ class MlSimMatrix:
             p = Process(target=pearson, args=(self.in_queue,self.out_queue))
             p.start()
             self.__threads.append(p)
-            
-        
+
+
     def __feedWorkers__(self):
         print "Building pairs"
         users = self.dataset.users(self.matrixSize)
@@ -145,11 +163,15 @@ class MlSimMatrix:
         self.feederLaunched = False
         print ''
         print "Done"
-    
+
 
 if __name__=="__main__":
     f = open('matrix.txt', 'w')
  #   f.write(str(MlSimMatrix().matrix))
-    mat = str(MlSimMatrix().matrix).replace(',', '\t').replace('))','').replace('(','')
-    f.write(mat)
-    f.close()
+    try:
+        obj = MlSimMatrix()
+        mat = str(obj.matrix).replace(',', '\t').replace('))','').replace('(','')
+        f.write(mat)
+        f.close()
+    except:
+        obj.kill()
