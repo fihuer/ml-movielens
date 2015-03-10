82c9bb01d9a66ea5161b8309b6d4c52d91e693f2
Fihue Romain <romain.fihue@student.ecp.fr>
2015-03-10 16:17:30 +0100

    Multiprocessing enhancements

--- pearson-sim.py
+++ pearson-sim.py
@@ -2,7 +2,7 @@
 
 from Orange import core
 from dataset import MlDataset
-from multiprocessing import Process, Queue, Pool
+from multiprocessing import Process, Manager, Pool
 import time, math
 from Queue import Full, Empty
 from threading import Thread
@@ -36,33 +36,54 @@ def pearson(in_queue, out_queue):
             result = 0
         out_queue.put((x, y, result), block=True, timeout=1)
 
+def feed(args):
+    xRange = args[0]
+    users = args[1]
+    dataset = args[2]
+    in_queue=args[3]
+    mean = args[4]
+
+    for x in xRange:
+        for y in users[x:]:
+            xmean = mean[x]
+            ymean = mean[y]
+            filtereddata = {movie: {user:  dataset.data[movie][user] for user in dataset.data[movie] if (user == x or user == y)} for \
+movie in dataset.data}
+            filtereddata = {movie: filtereddata[movie] for movie in filtereddata if filtereddata[movie]}
+            try:
+                in_queue.put((x,y, xmean, ymean, filtereddata), block=True, timeout=10)
+            except Full:
+                print ''
+                print "In Queue's full"
+
 class MlSimMatrix:
     def __init__(self):
         self.dataset = MlDataset("dataset/ml-100k.txt")
-        self.matrixSize = 943
+        self.matrixSize = 200
         self.feederLaunched = False
 
     @property
     def matrix(self):
         self.__startWorkers__()
-        thread = Thread(target=self.__feedWorkers__, args=())
-        thread.daemon = True
-        thread.start()
+        self.__startFeeder__()
         matrix = self.__gatherResults__()
         self.__stopWorkers__()
         return matrix
         
-    def __chunks__(self, users, csize):
-        for i in xrange(0, len(users), csize):
-            yield [users[i:i+csize], users, self.in_queue, self.dataset.mean]
+    def __chunks__(self, csize):
+        for i in xrange(0, self.matrixSize, csize):
+            yield [self.dataset.users(self.matrixSize)[i:i+csize], self.dataset.users(self.matrixSize), self.dataset ,self.in_queue, self.dataset.mean]
     
 
     def __startFeeder__(self, nbfeeders=4):
+        #thread = Thread(target=self.__feedWorkers__, args=())
+        #thread.daemon = True
+        #thread.start()
         p = Pool(nbfeeders)
-        chunks = tuple(self.__chunks__(self.dataset.users(self.matrixSize), self.matrixSize/nbfeeders))
+        chunks = tuple(self.__chunks__(self.matrixSize/nbfeeders))
 #        print chunks
-        p.map(self.__feedWorkers__, chunks)
-            
+        p.map_async(feed, chunks)
+        self.feederLaunched = True
 
     def __gatherResults__(self):
         while not self.feederLaunched:
@@ -72,12 +93,17 @@ class MlSimMatrix:
             print "No results yet"
             time.sleep(0.5)
         matrix = core.SymMatrix(self.matrixSize)
+        to_do = pow(self.matrixSize,2)/float(2)
+        done = 0
         while True:
             try:
                 result = self.out_queue.get(block=True, timeout=2)
+                done += 1
             except Empty:
                 break
             matrix[result[0], result[1]] = result[2]
+            if done%100==0:
+                print str(done/to_do)+'\r\r',
         return matrix
 
     def __stopWorkers__(self):
@@ -86,8 +112,9 @@ class MlSimMatrix:
             p.join(1)
 
     def __startWorkers__(self, nbworkers=4):
-        self.in_queue = Queue()
-        self.out_queue = Queue()
+        self.__manager = Manager()
+        self.in_queue = self.__manager.Queue()
+        self.out_queue = self.__manager.Queue()
         self.__threads = []
         for p in range(nbworkers):
             p = Process(target=pearson, args=(self.in_queue,self.out_queue))
